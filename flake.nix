ab483b90ed6c54606b8be9d7718faa5e2ee87237
Ask Sødal <asksodal@gmail.com>
2025-03-01 12:33:02 +0100

    maj: nix pnpm build and update

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [nodejs pnpm];
+          shellHook = "pnpm install --frozen-lockfile";
+        };
+        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "my-static-web";
+          version = "0-unstable-2025-03-01";
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm_9.configHook
+          ];
+          pnpmDeps = pkgs.pnpm_9.fetchDeps {
+            inherit (finalAttrs) pname version src;
+            hash = "sha256-KKRBqMBLs386QW6fgt0GS9f9nq/yzFSN59XZyazQqlM=";
+          };
+          buildPhase = ''
+            pnpm install --frozen-lockfile --offline
+            pnpm build
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp -r dist/* $out/
+          '';
+        });
+      }
+    );
+}
