8c60d0023822d49d2cf244c2efbf4d950bbfac3d
Ask Sødal <asksodal@gmail.com>
2025-08-10 19:04:36 +0200

    min: updated lock and added module for container
    Updated `flake.nix`.
    Added a module so website can be run in a `systemd-nspawn` container.

--- flake.nix
+++ flake.nix
@@ -1,42 +1,49 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   outputs = {
     self,
     nixpkgs,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [nodejs pnpm];
-          shellHook = "pnpm install --frozen-lockfile";
-        };
-        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = "website";
-          src = ./.;
-          version = "unstable";
-          nativeBuildInputs = [
-            pkgs.nodejs
-            pkgs.pnpm_9.configHook
-          ];
-          pnpmDeps = pkgs.pnpm_9.fetchDeps {
-            inherit (finalAttrs) pname version src;
-            hash = "sha256-KKRBqMBLs386QW6fgt0GS9f9nq/yzFSN59XZyazQqlM=";
-          };
-          buildPhase = ''
-            pnpm install --frozen-lockfile --offline
-            pnpm build
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r dist/* $out/
-          '';
-        });
-      }
-    );
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [nodejs pnpm];
+      shellHook = "pnpm install --frozen-lockfile";
+    };
+    packages.${system}.default = pkgs.stdenv.mkDerivation (finalAttrs: {
+      pname = "website";
+      src = ./.;
+      version = "unstable";
+      nativeBuildInputs = [
+        pkgs.nodejs
+        pkgs.pnpm_9.configHook
+      ];
+      pnpmDeps = pkgs.pnpm_9.fetchDeps {
+        inherit (finalAttrs) pname version src;
+        fetcherVersion = 1;
+        hash = "sha256-KKRBqMBLs386QW6fgt0GS9f9nq/yzFSN59XZyazQqlM=";
+      };
+      buildPhase = ''
+        pnpm install --frozen-lockfile --offline
+        pnpm build
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp -r dist/* $out/
+      '';
+    });
+    nixosModules.default = {...}: {
+      services.static-web-server = {
+        enable = true;
+        listen = "0.0.0.0:80";
+        root = self.packages.${system}.default;
+      };
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [80];
+      };
+      system.stateVersion = "24.05";
+    };
+  };
 }
